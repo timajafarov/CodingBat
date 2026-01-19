5347d02a2c3ee013a35b346fe719718bab1eca67
= <=>
2026-01-19 08:31:01 +0500

    Revert1

--- src/main/java/resetAndRevert/RevertDemo.java
+++
@@ -1,7 +0,0 @@
-package resetAndRevert;
-
-public class RevertDemo {
-    public static void main(String[] args) {
-        System.out.println("Demo1");
-    }
-}
---
+++ src/main/java/revertDemo/RevertDemo1.java
@@ -0,0 +1,7 @@
+package revertDemo;
+
+public class RevertDemo1 {
+    public static void main(String[] args) {
+        System.out.println("First one");
+    }
+}
